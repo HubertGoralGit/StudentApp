eb35a51245dbe4e5e11fdb4f1d17e798c8b25e1e
Hubert Góral <hubert.goral@inspirelabs.pl>
2020-05-14 12:20:01 +0200

    add react-calendar to project and styles to this

--- src/views/Todo/Todo.js
+++ src/views/Todo/Todo.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 import UserPageTemplate from '../../templates/UserPageTemplate';
 import Header from '../../components/Header/Header';
 import Heading from '../../components/Heading/Heading';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const StyledCalendarWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 25px;
+  margin-top: 50px;
+`;
 
 const Todo = () => (
   <UserPageTemplate pageType="todo">
     <Header>
       <Heading big>To-Do</Heading>
     </Header>
+    <StyledCalendarWrapper>
+      <Calendar />
+    </StyledCalendarWrapper>
   </UserPageTemplate>
 );
 
